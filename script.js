d781143c94e16443b9c55946a8469c2e26e6085b
AlexandraEvtush <89551088+ichtudirweh37@users.noreply.github.com>
2021-12-29 08:16:57 +0200

    lesson_5

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
+var tab;
+var tabContent;
 
-// function fun1() {
-//     var chbox;
-// chbox = document.getElementById('one');
+window.onload=function () {
+    tabContent=document.getElementsByClassName('tabContent');
+    tab=document.getElementsByClassName('tab');
+    hideTabsContent(1);
+}
 
-// if (chbox.checked) {
-//     alert('Выбран');
-// }
-// else
-// {
-//     alert('Не выбран');
-// }
-// }
+function hideTabsContent(a) {
+    for (var i=a; i<tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add('hide');
+        tab[i].classList.remove('whiteborder');
+    }
+}
 
-function fun1() {
-    var radi = document.getElementsByName('r1');
-    for (var i = 0 ; i < radi.length; i++) {
-        if (radi[i].checked) {
-            alert('Выбран ' +i+ ' элемент');
+document.getElementsById('tabs').onclick=function (event) {
+    var target=event.target;
+    if (target.className=='tab'){
+        for (var i=0; i<tab.length; i++) {
+            if(target == tab[i]){
+                showTabsContent(i);
+                break;
+            }
         }
     }
 }
 
+function showTabsContent(b) {
+    if (tabContent[b].classList.contains('hide')) {
+        hideTabsContent(0);
+        tab[b].classList.add('whiteborder');
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+    }
+}
