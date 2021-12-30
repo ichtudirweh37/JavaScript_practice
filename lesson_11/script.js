d57752fea6199bd88e9de1e09f881b45a646f180
AlexandraEvtush <89551088+ichtudirweh37@users.noreply.github.com>
2021-12-30 17:59:01 +0200

    Add files via upload

---
+++ lesson_11/script.js
@@ -0,0 +1,7 @@
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 50.40184915, lng: 30.53992534},
+            scrollwheel: true,
+            zoom: 5
+        });
+    }
